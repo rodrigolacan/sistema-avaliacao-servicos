5a6ddf65c2bbe88a4d3140d1a57fff297e072fb8
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-10 15:33:24 -0400

    feat: criando component de barra de pesquisa

---
+++ app/View/Components/util/SearchBar.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components\util;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class SearchBar extends Component
+{
+
+    /**
+     * Create a new component instance.
+     */
+    // Construtor para inicializar os parâmetros
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.util.search-bar');
+    }
+}
