4a13180d8a78ff6c0b649afef9595e4e71059edf
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-10 11:51:57 -0400

    refact: corrigindo rota para mostar o conteúdo da página herdada

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\contents\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [HomeController::class, 'home'])->name('index');
