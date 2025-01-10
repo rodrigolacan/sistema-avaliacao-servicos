0193c3c9741c2fbd202e6a0b437447ac53efa483
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-10 15:58:53 -0400

    feat: parametrizando e otimizando arquivos necessários

---
+++ app/Http/Controllers/Forms/PesquisaRequisicaoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\forms;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PesquisaRequisicaoController extends Controller
+{
+    public function retornarRequisicao(Request $request){
+
+    }
+}
