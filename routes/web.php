97f1575a093232daed74ef6e88975f6e9b923a31
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-10 16:14:18 -0400

    refact: criando rota para buscar uma requisição

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\Contents\AvaliacaoRequisicao;
+use App\Http\Controllers\Contents\AvaliacaoRequisicaoController;
 use App\Http\Controllers\Contents\HomeController;
+use App\Http\Controllers\Forms\PesquisaRequisicaoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('index');
-Route::get('/pesquisar/requisicoes', [AvaliacaoRequisicao::class, 'pesquisarRequisicao']);
+Route::get('/pesquisar/requisicoes', [AvaliacaoRequisicaoController::class, 'pesquisarRequisicao']);
+Route::get('search/requisicao', [PesquisaRequisicaoController::class, 'retornarRequisicao']);
