da2b6a095a4b9c3f06e24d565d2cee4f0e5b7795
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-10 15:09:36 -0400

    feat: criando rota para pesquisar requisicoes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\contents\AvaliacaoRequisicao;
 use App\Http\Controllers\contents\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('index');
+Route::get('/pesquisar/requisicoes', [AvaliacaoRequisicao::class, 'pesquisarRequisicao']);
