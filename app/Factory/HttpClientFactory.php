e998877a2fc11d76fb1ee684c66acefdf6ba243e
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-10 11:21:28 -0400

    feat: criando factory para a facade http do laravel

---
+++ app/Factory/HttpClientFactory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Factory;
+
+
+use Illuminate\Support\Facades\Http as FacadesHttp;
+
+class HttpClientFactory
+{
+    private string $baseUrl;
+    private array $defaultHeaders;
+
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+    /**
+     * Cria e retorna uma instância configurada do cliente HTTP.
+     *
+     * @param array $config Configurações adicionais para a requisição.
+     * @param string|null $token Token Bearer para autenticação, se necessário.
+     * @return \Illuminate\Http\Client\PendingRequest
+     */
+    public static function create(array $config = [], ?string $token = null)
+    {
+        // Inicia a instância do Http com a URL base (pode ser configurada se necessário)
+        $http = FacadesHttp::baseUrl(getenv('BASE_URL'));
+
+        // Se houver um token, adiciona ao cabeçalho de autorização
+        if ($token) {
+            $http = $http->withHeaders([
+                'Authorization' => 'Bearer ' . $token,
+            ]);
+        }
+
+        // Aplica qualquer configuração adicional fornecida
+        if (!empty($config)) {
+            foreach ($config as $key => $value) {
+                $http = $http->$key($value);
+            }
+        }
+
+        return $http;
+    }
+}
