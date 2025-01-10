f13ae43c54a0b3abfb9f038f102dd80079907089
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-10 11:51:13 -0400

    feat: Alterando referencia para a view no controller

--- app/Http/Controllers/contents/HomeController.php
+++ app/Http/Controllers/contents/HomeController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home(){
-        return view('welcome');
+        return view('contents.avaliacao');
     }
 }
