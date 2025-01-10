d25b757e6e89191e777362b4119e3031736f11bb
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-10 15:34:22 -0400

    refact: concertando namespace dos controllers da pasta contents

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\contents\AvaliacaoRequisicao;
-use App\Http\Controllers\contents\HomeController;
+use App\Http\Controllers\Contents\AvaliacaoRequisicao;
+use App\Http\Controllers\Contents\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('index');
