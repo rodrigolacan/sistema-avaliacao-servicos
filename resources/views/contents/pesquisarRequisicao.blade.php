1f3b0e31d83be9f9c3902a4885776f7a04b11cde
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-13 08:13:18 -0400

    refact: ajustando action conforme a rota

--- resources/views/contents/pesquisarRequisicao.blade.php
+++ resources/views/contents/pesquisarRequisicao.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.app')
 
 @section('pesquisarRequisicao-body')
-        <x-util.search-bar method="GET" action="/search/requisicao" />
+    <x-util.search-bar method="GET" action="/search/requisicao" />
 @endsection
