75f0db48b7fe9bb3d48fbc631a351cfeda42f246
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-14 09:22:41 -0400

    feat: criando botão de avaliar requisição

--- resources/views/components/requisicao/table-requisicao.blade.php
+++ resources/views/components/requisicao/table-requisicao.blade.php
@@ -61,12 +61,11 @@
                                 </td>
 
                                 <td class="px-4 py-4 text-sm whitespace-nowrap">
-                                    <button class="px-1 py-1 text-gray-500 transition-colors duration-200 rounded-lg dark:text-gray-300 hover:bg-gray-100">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z" />
-                                        </svg>
+                                    <button 
+                                        class="px-4 py-2 text-white transition-colors duration-200 rounded-lg bg-emerald-500 hover:bg-emerald-400 focus:outline-none dark:bg-emerald-600 dark:hover:bg-emerald-500">
+                                        Avaliar Requisição
                                     </button>
-                                </td>
+                                </td>                                                             
                             </tr>
                         </tbody>
                     </table>
