085e3cad5a6fc97c97881b74752bc531cec3ed19
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-14 14:03:03 -0400

    feat: criando rota post para forms de avaliação

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\Contents\AvaliacaoRequisicaoController;
 use App\Http\Controllers\Contents\HomeController;
+use App\Http\Controllers\Forms\EnviarPesquisaAvaliacaoController;
 use App\Http\Controllers\Forms\PesquisaRequisicaoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'home'])->name('index');
 Route::get('/pesquisar/requisicoes', [AvaliacaoRequisicaoController::class, 'pesquisarRequisicao'])->name('pesquisar.requisicao');
-Route::get('search/requisicao', [PesquisaRequisicaoController::class, 'retornarRequisicao']);
+Route::get('/search/requisicao', [PesquisaRequisicaoController::class, 'retornarRequisicao']);
+Route::post('/forms/avaliacao', [EnviarPesquisaAvaliacaoController::class, 'enviarPesquisa'])->name('enviar.avaliacao');
