2a495526e7760bb6ee28f3f912183c2c13720865
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-17 13:23:02 -0400

    feat: criando component table para listar as avaliações

--- app/Http/Controllers/Contents/PesquisarAvalicaoController.php
+++ app/Http/Controllers/Contents/PesquisarAvalicaoController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Contents;
 
 use App\Http\Controllers\Controller;
+use App\Models\Avaliacao;
 use Illuminate\Http\Request;
 
 class PesquisarAvalicaoController extends Controller
 {
-    public function pesquisarAvaliacao(){
-        return view('contents.pesquisarAvaliacao');
+    public function pesquisarAvaliacao(Request $request){
+
+        $avaliacoes = Avaliacao::with('requisicao')->paginate(10);
+        return view('contents.pesquisarAvaliacao',compact('avaliacoes'));
     }
 }
