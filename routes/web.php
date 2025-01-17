0b9fd833eda7faedb321a42f8af4c5dd67e828e3
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-17 09:41:18 -0400

    feat: criando rota get para listar as avaliações

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Contents\AvaliacaoRequisicaoController;
 use App\Http\Controllers\Contents\HomeController;
+use App\Http\Controllers\Contents\PesquisarAvalicao;
 use App\Http\Controllers\Forms\EnviarPesquisaAvaliacaoController;
 use App\Http\Controllers\Forms\PesquisaRequisicaoController;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,5 @@ Route::get('/', [HomeController::class, 'home'])->name('index');
 Route::get('/pesquisar/requisicoes', [AvaliacaoRequisicaoController::class, 'pesquisarRequisicao'])->name('pesquisar.requisicao');
 Route::get('/search/requisicao', [PesquisaRequisicaoController::class, 'retornarRequisicao']);
 Route::post('/forms/avaliacao', [EnviarPesquisaAvaliacaoController::class, 'enviarPesquisa'])->name('enviar.avaliacao');
+
+Route::get('/pesquisar/avaliacoes',[PesquisarAvalicao::class, 'pesquisarAvaliacao'])->name('pesquisar.avaliacao');
