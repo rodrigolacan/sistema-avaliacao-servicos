fa5f395b31e5a1eece57fbb5b4d9d69d3fb958bf
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-21 14:34:51 -0400

    refact: removendo redirecionamento http para https

--- docker/script-ssl.sh
+++ docker/script-ssl.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Configurar o VirtualHost para redirecionar HTTP para HTTPS
+# Configurar o VirtualHost HTTP
 echo "<VirtualHost *:80>
+    ServerAlias avaliacao-homolog.rr.sebrae.com.br
     ServerName avaliacao-homolog.rr.sebrae.com.br
-    Redirect permanent / https://avaliacao-homolog.rr.sebrae.com.br/
+    DocumentRoot /var/www/html/public
+    <Directory /var/www/html/public>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>" > /etc/apache2/sites-available/000-default.conf
 
 # Configurar o VirtualHost para HTTPS
