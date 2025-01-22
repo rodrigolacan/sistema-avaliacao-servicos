adf294bcbbcd4b6680cf6432053371b6fb50c150
Rodrigo Lacan <rodrigo.lacan@rr.sebrae.com.br>
2025-01-22 10:43:21 -0400

    refact: criando teste básico

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,4 +10,10 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
 }
